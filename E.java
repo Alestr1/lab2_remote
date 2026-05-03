07431b45605b09b123ab1aa9206e3879d8ac53ba
red <red@gmail.com>
2026-05-03 15:39:45 +0300

    r0

---
+++ E.java
@@ -0,0 +1,6 @@
+public interface E {
+
+    java.util.Random mm();
+
+    void ab();
+}
---
+++ G.java
@@ -0,0 +1,30 @@
+public class G implements K, E {
+
+    private byte i = 1;
+
+    private String a = "hello";
+
+    public String kk() {
+        return "No";
+    }
+
+    public double ee() {
+        return 500.100;
+    }
+
+    public Object pp() {
+        return this;
+    }
+
+    public long ac() {
+        return 222;
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public void ab() {
+        System.out.println("\n");
+    }
+}
